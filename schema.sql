ab1d17796a07bc1573ef0102f292e5ccda259ccf
Nizar Kamel <nizarekamel@gmail.com>
2022-04-05 18:12:56 -0400

    query code and new data

--- schema.sql
+++ schema.sql
@@ -33,5 +33,22 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+CREATE TABLE dept_emp (
+  dept_no varchar NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (dept_no, emp_no)
+);
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
 
 Select * FROM departments;
